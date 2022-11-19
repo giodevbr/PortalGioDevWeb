3995d969b30733c42bf3aaa674b7b8f79f1b7afb
Giovanne da Silva Batista <giobatista14@gmail.com>
2022-11-19 17:48:27 -0400

    - Correção do nome do App

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PortalGioDevWeb'`, () => {
+  it(`should have as title 'Portal GioDev'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('PortalGioDevWeb');
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PortalGioDevWeb app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Portal GioDev app is running!');
   });
 });
